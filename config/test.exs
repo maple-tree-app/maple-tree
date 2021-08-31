5149e934f465e5bbd0d9fd33376c0c2e6561a321
pedro vietro <hammsvietro@gmail.com>
2021-08-30 21:42:26 -0300

    fixes tests

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,7 @@ config :bcrypt_elixir, :log_rounds, 1
 config :maple, Maple.Repo,
   username: "postgres",
   password: "postgres",
-  database: "maple_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "maple_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
