a3ecd1538b6b4044e4f7073a20edce53cc639116
pedro vietro <hammsvietro@gmail.com>
2021-09-10 22:10:50 -0300

    rename folders

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,7 +35,6 @@ if(!isProduction) {
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
-
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
