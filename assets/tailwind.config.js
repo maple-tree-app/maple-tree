7adbeffe92829cc48ebfb12c04064c5efc48b551
pedro vietro <hammsvietro@gmail.com>
2021-09-15 22:35:01 -0300

    finish base group card component

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -12,12 +12,14 @@ module.exports = {
       colors: {
         'maple-dark-bg': '#212121',
         'maple-light-bg': '#E7EDF3',
-        'maple-dark-border': '#8f8f8f',
-        'maple-light-border': '#A7AFB2',
+        'maple-dark-border': '#A7AFB2',
+        'maple-light-border': '#8f8f8f',
         'maple-dark-input-bg': '#474747',
         'maple-red': '#EF4343',
         'maple-yellow': '#FBAF0A',
-        'maple-navbar-dark': '#3333'
+        'maple-navbar-dark': '#3333',
+        'maple-dark-toggle-bg': '#2222',
+        'maple-dark-toggle-border': '#403f3f'
       }
     },
   },
