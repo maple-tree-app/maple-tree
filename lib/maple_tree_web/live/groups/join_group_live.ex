feb7339a7a977c663832a6960f90da6b79ccb1f2
hammsvietro <49427492+hammsvietro@users.noreply.github.com>
2021-10-09 20:16:36 -0300

    Base groups feature (#1)
    * finish join group card
    
    * fixes tests

--- lib/maple_tree_web/live/groups/join_group_live.ex
+++ lib/maple_tree_web/live/groups/join_group_live.ex
@@ -7,12 +7,19 @@ defmodule MapleTreeWeb.JoinGroupLive do
 
   @impl true
   def mount(%{"code" => invite_code}, session, socket) do
+    IO.inspect Groups.get_group_by_invite_code(invite_code)
     {:ok, socket |> LiveHelpers.init(session) |> assign(group: Groups.get_group_by_invite_code(invite_code))}
   end
 
   @impl true
   def render(assigns) do
     View.render(MapleTreeWeb.GroupsView, "join.html", assigns)
+  end
+
+  @impl true
+  def handle_event("join-group", _, socket) do
+    Groups.add_user(socket.assigns.group.id, socket.assigns.current_user.id)
+    {:noreply, push_redirect(socket, to: Routes.groups_details_path(socket, :show, socket.assigns.group.id))}
   end
 
 end
