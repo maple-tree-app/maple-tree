28e503cd9d2a0f281f92646d52475f96651c6a97
hammsvietro <hammsvietro@gmail.com>
2021-11-11 21:39:39 -0300

    starts item page

--- lib/maple_tree_web/components/shopping_list_details_table.ex
+++ lib/maple_tree_web/components/shopping_list_details_table.ex
@@ -4,8 +4,19 @@ defmodule MapleTreeWeb.Components.ShoppingListDetailsTable do
 
   def main_table(assigns) do
     ~H"""
-    <h1>TEST!!!</h1>
+    <div class="rounded-md border border-black dark:border-gray-400 w-full h-40 grid grid-cols-3">
+      <.add_new_item />
+      <.add_new_item />
+      <.add_new_item />
+    </div>
+    """
+  end
 
+  def add_new_item(assigns) do
+    ~H"""
+    <div class="flex items-center justify-center">Item</div>
+    <div class="flex items-center justify-center">Quantity</div>
+    <div class="flex items-center justify-center">Details</div>
     """
   end
 end
